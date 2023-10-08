a49c14400a09a00092299014d21953af40fbd10e
Muhammad Hassan <muhammadhsn09@gmail.com>
2023-10-08 22:17:07 +0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { DiscoverComponent } from '../components/discover/discover.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, DiscoverComponent, FooterComponent],
 })
 export class HomePage {
   constructor() {}
